956d2e39c0629f0e78d1a1f6ce78ba7c75d21728
Javier <jmanteca@jmanvel.com>
2021-10-14 13:25:49 +0000

    finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { TrafficLight } from "./trafficLight.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -7,18 +8,11 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
+			<h1>Chavi Traffic Light</h1>
 			<p>
 				<img src={rigoImage} />
 			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<TrafficLight />
 		</div>
 	);
 };
